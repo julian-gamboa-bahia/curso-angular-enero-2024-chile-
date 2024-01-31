76b95dd02c599b0534268af09501d03d61e4831c
julian-gamboa-bahia <mais.seguranca.android@gmail.com>
2024-01-31 13:42:24 -0300

    usar o boostrap

--- clase_4/src/app/app.component.ts
+++ clase_4/src/app/app.component.ts
@@ -2,10 +2,27 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { FooterComponent } from './componentes/footer/footer.component'; // Footer
+import { NavbarComponent } from '././componentes/navbar/navbar.component'; // navbar
+
+
+import { NewsletterComponent } from './componentes/newsletter/newsletter.component'; // 
+import { RecipeComponent } from '././componentes/recipe/recipe.component'; // 
+
+/*
+
+*/
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, 
+    FooterComponent,
+    NavbarComponent,
+    NewsletterComponent,
+    RecipeComponent    
+  ], // Add imported components to the imports array
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
